9b9285dcb6ccd70103c97ac563f86c4a7885a1a6
czelabueno <c.zelabueno@gmail.com>
2023-08-11 20:33:36 +0000

    changing build.sh to add docker file for distroless container

--- Serie2-DevelopingCloudJavaApp/spring-aot/build.sh
+++ Serie2-DevelopingCloudJavaApp/spring-aot/build.sh
@@ -1,3 +1,5 @@
 rm -rf target
 ./mvnw clean -Pnative native:compile
 # ./gradlew nativeCompile
+
+docker build -f Dockerfile.distroless --build-arg APP_FILE=target/spring-aot-ni -t  local/spring-aot:distroless .
