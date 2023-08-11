dd0e831cd17eadd660d95d0ea2f1c1764edc4e79
czelabueno <c.zelabueno@gmail.com>
2023-08-11 04:53:06 +0000

    adding setup musl and get ecs task public dns

--- Serie2-DevelopingCloudJavaApp/spring-aot/build.sh
+++ Serie2-DevelopingCloudJavaApp/spring-aot/build.sh
@@ -1,3 +1,8 @@
+TOOLCHAIN_DIR=`pwd`/x86_64-linux-musl-native
+CC=${TOOLCHAIN_DIR}/bin/gcc
+PATH=${TOOLCHAIN_DIR}/bin:${PATH}
+
 rm -rf target
 ./mvnw clean -Pnative native:compile
 # ./gradlew nativeCompile
+docker build -f Dockerfile.scratch --build-arg APP_FILE=target/spring-aot-ni -t  local/spring-aot:scratch .
