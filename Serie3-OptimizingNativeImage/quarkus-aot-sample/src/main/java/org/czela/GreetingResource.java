3283cd1276030a0ce758da98eb82d55af9a2df67
czelabueno <c.zelabueno@gmail.com>
2023-08-30 22:28:11 -0400

    quarkus aot sample application with native image

---
+++ Serie3-OptimizingNativeImage/quarkus-aot-sample/src/main/java/org/czela/GreetingResource.java
@@ -0,0 +1,18 @@
+package org.czela;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
